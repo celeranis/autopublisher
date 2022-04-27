516bf53b70195757d20e63fcdaca67ac87decfb4
celeranis <contact@celeranis.dev>
2022-04-26 20:16:27 -0500

    Some backwards compatability Provides a redirect for anyone previously relying on `node index`

---
+++ index.cjs
@@ -0,0 +1,9 @@
+/*
+	This file attempts to allow backwards compatability 
+	for anyone previously self-hosting the bot.
+	
+	If you're not one of those people, it's best to start the bot
+	with `pnpm start` (or the pm2 ecosystem file) instead.
+*/
+
+import('./src/standalone.js')
